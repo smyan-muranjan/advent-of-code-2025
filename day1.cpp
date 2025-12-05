f9d32d191c3863e38a16aad40c26d16093581ab0
smyan-muranjan <smyan.muranjan@gmail.com>
2025-12-05 15:55:16 -0500

    Minor Formatting Changes Day 1

--- day1.cpp
+++ day1.cpp
@@ -7,21 +7,26 @@ int partOne() {
     int curr = 50;
     std::string line;
     std::ifstream inputFile("day1.txt");
-    if (inputFile.is_open()) {
-        while (std::getline(inputFile, line)) {
-            char dir = line[0];
-            int len = std::stoi(line.substr(1));
-            if (dir == 'R') {
-                curr = (curr + len) % 100;
-            } else { // dir == 'L'
-                curr = (curr + 100 - len) % 100;
-            }
-            if (curr == 0) {
-                res++;
-            }
-        }
-        inputFile.close();
+
+    if (!inputFile.is_open()) {
+        return -1;
     }
+
+    while (std::getline(inputFile, line)) {
+        char dir = line[0];
+        int len = std::stoi(line.substr(1));
+
+        if (dir == 'R') {
+            curr = (curr + len) % 100;
+        } else { // dir == 'L'
+            curr = (curr + 100 - len) % 100;
+        }
+        if (curr == 0) {
+            res++;
+        }
+    }
+
+    inputFile.close();
     return res;
 }
 
@@ -30,25 +35,30 @@ int partTwo() {
     int curr = 50;
     std::string line;
     std::ifstream inputFile("day1.txt");
-    if (inputFile.is_open()) {
-        while (std::getline(inputFile, line)) {
-            char dir = line[0];
-            int len = std::stoi(line.substr(1));
-            if (dir == 'R') {
-                res += (curr + len) / 100;
-                curr = (curr + len) % 100;
-            } else { // dir == 'L'
-                int distToZero = (curr == 0) ? 100 : curr;
-                if (len >= distToZero) {
-                    res += 1 + (len - distToZero) / 100;
-                }
-                
-                int rawPos = (curr - len) % 100;
-                curr = (rawPos < 0) ? rawPos + 100 : rawPos;
-            }
-        }
-        inputFile.close();
+
+    if (!inputFile.is_open()) {
+        return -1;
     }
+    
+    while (std::getline(inputFile, line)) {
+        char dir = line[0];
+        int len = std::stoi(line.substr(1));
+
+        if (dir == 'R') {
+            res += (curr + len) / 100;
+            curr = (curr + len) % 100;
+        } else { // dir == 'L'
+            int distToZero = (curr == 0) ? 100 : curr;
+            if (len >= distToZero) {
+                res += 1 + (len - distToZero) / 100;
+            }
+            
+            int rawPos = (curr - len) % 100;
+            curr = (rawPos < 0) ? rawPos + 100 : rawPos;
+        }
+    }
+
+    inputFile.close();
     return res;
 }
 
