b87796e2a5c5bec18a92100af13d98a06edabcd5
smuranjan1 <smyan.muranjan@gmail.com>
2025-12-05 21:37:37 -0500

    Finished Day 3 (Skipped 2D DP optimization)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Advent of Code 2025
-| Day | Part 1 | Part 2 |
-|-----|--------|--------|
-|  1  |   ✅    |   ✅    |
-|  2  |   ✅    |   ✅    |
+| Day | Part 1 | Part 2 | Notes |
+|-----|--------|--------|-------|
+|  1  |   ✅    |   ✅    | |
+|  2  |   ✅    |   ✅    | |
+|  3  |   ✅    |   ✅    | Part 2 can be optimized using 2D DP |
---
+++ day3.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int partOne() {
+    std::fstream inputFile("day3.txt");
+    if (!inputFile.is_open()) {
+        return -1;
+    }
+    
+    std::string line;
+    int res = 0;
+
+    while (std::getline(inputFile, line)) {
+        int max_digit = -1;
+        int curr_max = 0;
+
+        for (char c : line) {
+            int digit = c - '0';
+
+            int voltage = (max_digit * 10) + digit;
+            if (voltage > curr_max) {
+                curr_max = voltage;
+            }
+            if (digit > max_digit) {
+                max_digit = digit;
+            }
+        }
+        res += curr_max;
+    }
+
+    return res;
+}
+
+long dfs(std::string curr, std::string &line, int idx) {
+    if (curr.length() == 12) {
+        return std::stol(curr);
+    }
+    int digits_left = 12 - curr.length();
+    long max = -1;
+    for (int i = idx; i <= line.length() - digits_left; i++) {
+        std::string next = curr + line[i];
+        long val = dfs(next, line, i + 1);
+        if (val > max) {
+            max = val;
+        }
+    }
+    return max;
+}
+
+long partTwoDfs() {
+    std::fstream inputFile("day3.txt");
+    if (!inputFile.is_open()) {
+        return -1;
+    }
+    
+    std::string line;
+    long res = 0;
+
+    while (std::getline(inputFile, line)) {
+        res += dfs("", line, 0);
+    }
+    
+    return res;
+}
+
+int main() {
+    std::cout << "Part One: " << partOne() << std::endl;
+    std::cout << "Part Two: " << partTwoDfs() << std::endl;
+    return 0;
+}
